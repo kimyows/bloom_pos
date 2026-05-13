8367786b5bcddf22521c68524c9d9d9890ea2008
curlize <mojares.charlize@gmail.com>
2026-05-13 18:52:15 +0800

    update

--- bloom.php
+++ bloom.php
@@ -13,7 +13,7 @@ if (!is_dir("uploads")) {
 
 date_default_timezone_set("Asia/Manila");
 
-include 'Product.inc.php';
+include 'Product.inc.php'; //kahit ano
 $sampleProducts = [
   'flowers'     => new FlowerProduct('FLW-001', 'Red Roses', 99.00, 50),
   'arrangement' => new ArrangementProduct('ARR-001', 'Bridal Bouquet', 599.00, 10),
